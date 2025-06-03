a669aef1c7d0c8fd105ddb8c8477f4c77ae6efbd
Laura <lauracast142@gmail.com>
2025-06-03 10:34:32 -0500

    com

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRippleEffect, IonFabButton, IonFab } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square, shield } from 'ionicons/icons';
+import { triangle, ellipse, square, shield,homeOutline, starOutline, locationOutline, calendarOutline} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +13,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square,shield });
+    addIcons({ triangle, ellipse, square,shield,homeOutline, starOutline, locationOutline, calendarOutline });
   }
 }
